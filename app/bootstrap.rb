3372d035223c776f5c6d84305c5f9dfff7f28ed5
Vredniy <dnd.pliz@gmail.com>
2012-02-04 01:32:46 +0400

    page model created

--- app/bootstrap.rb
+++ app/bootstrap.rb
@@ -2,6 +2,24 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 
+DataMapper.setup(:default, ENV['DATABASE_URL'])
+
+class Page
+  include DataMapper::Resource
+
+  property :id,               Serial
+  property :name,             String
+  property :short,            Text
+  property :full,             Text
+  property :seo_title,        String
+  property :seo_keywords,     String
+  property :seo_description,  String
+  property :created_at,       Datetime
+end
+
+DataMapper.auto_upgrade!
+
+
 get '/' do
-	erb "Hello, world, at #{Time.now}"
+  erb "Hello, world, at #{Time.now}"
 end
